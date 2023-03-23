65c9637fac670a3edc3b2f1700505a16b29ed8da
Kundan Bhosale <62865281+kundanbhosale@users.noreply.github.com>
2023-03-23 18:51:31 +0530

    Update how-to-use.tsx

--- pages/how-to-use.tsx
+++ pages/how-to-use.tsx
@@ -7,6 +7,8 @@ const HowToUse = () => {
         <div>
             <NavBar />
             <h1 style={{ textAlign: 'center' }}>How To Use</h1>
+            <div style={{width:"100%", height:'100%', display:'flex', alignItems:'center', justifyContent:'center'}}>
+            </div>
                   <iframe width="560" height="315" src="https://www.youtube.com/embed/HQVX0LhaA0o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
     )
