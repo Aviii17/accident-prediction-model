e5bc4da5900246f7199e27d273ad1793fc964e63
Kundan Bhosale <62865281+kundanbhosale@users.noreply.github.com>
2023-03-23 19:16:21 +0530

    Update how-to-use.tsx

--- pages/how-to-use.tsx
+++ pages/how-to-use.tsx
@@ -10,7 +10,7 @@ const HowToUse = () => {
             <div style={{width:"100%", height:'100%', display:'flex', alignItems:'center', justifyContent:'center'}}>
 
                   <iframe width="560" height="315" src="https://www.youtube.com/embed/HQVX0LhaA0o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3j1rU31vVMid4ma_7BIvXnkbE_OrHz7KykpeqLht8HOShxA/viewform?embedded=true" width="640" height="947" frameborder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3j1rU31vVMid4ma_7BIvXnkbE_OrHz7KykpeqLht8HOShxA/viewform?embedded=true" width="640" height="947" >Loading…</iframe>
             </div>
         </div>
     )
