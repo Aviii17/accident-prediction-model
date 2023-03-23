9547e347020943e5c476fc03ae97271fd78d008b
Kundan Bhosale <62865281+kundanbhosale@users.noreply.github.com>
2023-03-23 19:24:06 +0530

    Update contact-us.tsx

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -6,10 +6,12 @@ const ContactUs = () => {
     <div>
       <NavBar />
       <h1 style={{ textAlign: 'center' }}>Contact Us</h1>
+            <div style={{width:"100%", height:'100%', display:'flex', alignItems:'center', justifyContent:'center'}}>
 
                   
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3j1rU31vVMid4ma_7BIvXnkbE_OrHz7KykpeqLht8HOShxA/viewform?embedded=true" width="1100" height="1500" >Loading…</iframe>
-    </div>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3j1rU31vVMid4ma_7BIvXnkbE_OrHz7KykpeqLht8HOShxA/viewform?embedded=true" width="650" height="1000" >Loading…</iframe>
+      </div>
+      </div>
   )
 }
 
