c021102fc5af91c4bcf11b513fa7feaad84a8af0
Aviii17 <79978112+Aviii17@users.noreply.github.com>
2023-03-23 19:22:22 +0530

    Update contact-us.tsx

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -7,8 +7,8 @@ const ContactUs = () => {
       <NavBar />
       <h1 style={{ textAlign: 'center' }}>Contact Us</h1>
 
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/HQVX0LhaA0o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3j1rU31vVMid4ma_7BIvXnkbE_OrHz7KykpeqLht8HOShxA/viewform?embedded=true" width="640" height="1500" >Loading…</iframe>
+                  
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3j1rU31vVMid4ma_7BIvXnkbE_OrHz7KykpeqLht8HOShxA/viewform?embedded=true" width="1100" height="1500" >Loading…</iframe>
     </div>
   )
 }
