fcf8f22c24dbdd89f78a2cefb78bed90400a104d
Aviii17 <79978112+Aviii17@users.noreply.github.com>
2023-03-23 19:13:50 +0530

    Update how-to-use.tsx

--- pages/how-to-use.tsx
+++ pages/how-to-use.tsx
@@ -10,7 +10,8 @@ const HowToUse = () => {
             <div style={{width:"100%", height:'100%', display:'flex', alignItems:'center', justifyContent:'center'}}>
 
                   <iframe width="560" height="315" src="https://www.youtube.com/embed/HQVX0LhaA0o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      </div>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3j1rU31vVMid4ma_7BIvXnkbE_OrHz7KykpeqLht8HOShxA/viewform?embedded=true" width="640" height="947" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
     )
 }
